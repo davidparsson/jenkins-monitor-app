479fcc1074d0cda7268cbb249c7006cc2196bbaf
David Pärsson <david@parsson.se>
2018-10-17 11:20:25 +0200

    Add minimum window size

--- jenkins-monitor.sh
+++ jenkins-monitor.sh
@@ -1,1 +1,1 @@
-nativefier --internal-urls='^((?!/job/).)*$' --fast-quit --single-instance --icon=headshot.png $1
+nativefier --internal-urls='^((?!/job/).)*$' --fast-quit --single-instance --icon=headshot.png --min-width=240 --min-height=180 $1

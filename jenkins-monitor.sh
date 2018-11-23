d7445d67354d94541c4ede2fb0275998f34aef8f
David Pärsson <david@parsson.se>
2018-11-23 09:52:04 +0100

    Arguments on new lines

--- jenkins-monitor.sh
+++ jenkins-monitor.sh
@@ -1,1 +1,8 @@
-./node_modules/.bin/nativefier --internal-urls='^((?!/job/).)*$' --fast-quit --single-instance --icon=headshot.png --min-width=240 --min-height=180 $1
+./node_modules/.bin/nativefier \
+  --internal-urls='^((?!/job/).)*$' \
+  --fast-quit \
+  --single-instance \
+  --icon=headshot.png \
+  --min-width=240 \
+  --min-height=180 \
+  $1
